b3c8ac88b8c30f4004304697cc8d3a466e3bd44f
Tasdeed Aziz <ayonaziz@gmail.com>
2022-09-16 16:11:20 -0400

    server updates added bmi route

--- calculator.js
+++ calculator.js
@@ -20,6 +20,21 @@ app.post("/", function (req, res) {
   res.send("The result of the calculation is: " + result);
 });
 
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  console.log(req.body);
+
+  let weight = parseFloat(req.body.num1);
+  let height = parseFloat(req.body.num2);
+
+  let result = weight / Math.pow(height, 2);
+
+  res.send("The result of the calculation is: " + result);
+});
+
 app.listen(3000, () => {
   console.log(`Server 🚀 starting on port ${port}`);
 });
